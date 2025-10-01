1623ce45ca1d544f9b6012548f98ea48e304dfac
Friedrich Waack <friedrich.waack@programado.de>
2025-10-01 15:00:39 +0200

    Add UpdateGraphQLDevSchema to KomandoServiceProvider
    - Register UpdateGraphQLDevSchema command for use.

--- src/Providers/KomandoServiceProvider.php
+++ src/Providers/KomandoServiceProvider.php
@@ -4,6 +4,7 @@ namespace Programado\Komando\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Programado\Komando\Console\Commands\SyncDatabaseCommand;
+use Programado\Komando\Console\Commands\UpdateGraphQLDevSchema;
 
 final class KomandoServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,7 @@ final class KomandoServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__ . '/../../config/komando.php', 'komando');
         $this->commands(SyncDatabaseCommand::class);
+        $this->commands(UpdateGraphQLDevSchema::class);
     }
 
     /**
